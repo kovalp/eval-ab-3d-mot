a563940e6cb13aedd60706941be2da1abb8fecdf
Peter Koval <koval.peter@gmail.com>
2025-10-08 15:38:12 +0200

    improve the cli help

--- src/eval_ab_3d_mot/cli/ab_3d_mot/cmd_line.py
+++ src/eval_ab_3d_mot/cli/ab_3d_mot/cmd_line.py
@@ -5,15 +5,17 @@ from typing import Sequence
 
 from rich_argparse import RawTextRichHelpFormatter
 
+from eval_ab_3d_mot.cli.common import get_hlp
 
 PROG = 'run-ab-3d-mot'
+HLP_OUT = 'File name to store tracking results.'
 
 
 class CmdLineRunAb3dMot:
     def __init__(self) -> None:
         self.verbosity = 0
         self.det_file_name = ''
-        self.trk_file_name = 'tracking-kitti-info.txt'
+        self.trk_file_name = 'tracking-kitti.txt'
 
 
 def get_cmd_line(args: Sequence[str]) -> CmdLineRunAb3dMot:
@@ -21,6 +23,6 @@ def get_cmd_line(args: Sequence[str]) -> CmdLineRunAb3dMot:
     parser = ArgumentParser(PROG, f'{PROG} [OPTIONS]', formatter_class=RawTextRichHelpFormatter)
     parser.add_argument('det_file_name', help='File name with detections.')
     parser.add_argument('--verbosity', '-v', action='count', help='Script verbosity.')
-    parser.add_argument('--trk-file-name', '-o', help='File name to store tracking results.')
+    parser.add_argument('--trk-file-name', '-o', help=get_hlp(HLP_OUT, cli.trk_file_name))
     parser.parse_args(args, namespace=cli)
     return cli
